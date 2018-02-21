248b47c6bac8db25d211ab5a268bb501a10a0404
Benjamin Thint <benjamin.thint@gmail.com>
2018-02-21 16:45:45 +0900

    complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,14 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#   Character.create(name: 'Luke', movie: movies.first
+
+category = %w[chinese italian japanese french belgian]
+10.times do
+  Restaurant.create(name:Faker::Zelda.character, address: Faker::Address.city, phone_number: Faker::PhoneNumber.phone_number, category: category.sample)
+end
+
+all_restaurants = Restaurant.all
+50.times do
+  all_restaurants.sample.reviews << Review.new(content: Faker::RickAndMorty.quote, rating: rand(0..5))
+end
